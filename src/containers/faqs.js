4dd6aece761f288551cca35cec6f7632fb82ef81
Andrew Talle <talle.andrew@gmail.com>
2020-11-18 12:51:03 -0800

    feat: add optform to accordion

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
@@ -14,6 +14,15 @@ export function FaqsContainer() {
           </Accordion.Item>
         ))}
       </Accordion.Frame>
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
