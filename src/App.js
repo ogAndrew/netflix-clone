8f30b8e178982748cb2f741ee7bf3dcb867ebf2d
Andrew Talle <talle.andrew@gmail.com>
2020-11-18 19:54:29 -0800

    feat: complete header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
-
 import * as ROUTES from './constants/routes';
 
 export default function App() {
