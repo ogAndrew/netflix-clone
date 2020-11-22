334e1b9bbc3bcadb048b94fafb43b7b3738db358
Andrew Talle <talle.andrew@gmail.com>
2020-11-21 16:00:28 -0800

    feat: create custom hooks to hydrate application with data

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+  // we need the series and films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(useContent('series'));
+  // we need slides
+  // pass it to browse container
+
   return (
     <div>
       <h2>Hello from Browse</h2>
